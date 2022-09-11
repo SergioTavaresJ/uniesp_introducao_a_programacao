f8f944eea3e1a5a2a40478e947f6d90253947be4
SergioTavares <112196923+SergioTavaresJ@users.noreply.github.com>
2022-09-11 19:29:35 -0300

    Add files via upload

---
+++ Aula 03/atividades/Aula03 Q04.py
@@ -0,0 +1,3 @@
+nome = input("Digite o nome do convidado: ")
+
+print(" Voce, {},foi convidado para minha festa.".format(nome.upper()))
---
+++ Aula 03/atividades/aula 03 Q01.py
@@ -0,0 +1,2 @@
+mensagem = " Eu amo programa em Python ! "
+print(mensagem)
