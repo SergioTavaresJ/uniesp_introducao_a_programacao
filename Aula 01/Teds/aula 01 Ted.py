7180dfe7d80c119a57ea2ed2c9d9a3103893ab02
SergioTavares <112196923+SergioTavaresJ@users.noreply.github.com>
2022-09-12 12:45:28 -0300

    Add files via upload

---
+++ Aula 01/Teds/aula 01 Ted.py
@@ -0,0 +1,5 @@
+Qual a importância de se estudar Lógica de Programação e Algoritmos para o profissional de TI?
+
+R-O estudo da lógica de programação e algoritmos é indispensável, pois através da lógica podemos solucionar problemas utilizando uma linguagem por meio de códigos
+que será interpretada pelo computador,assim organizando-o para poder executar o algoritmo,seguindo uma sequência,um "manual" para realização de uma ou mais
+funções.
