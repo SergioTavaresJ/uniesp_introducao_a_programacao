0e55d3ad8fada99c334450792e1d46fe20d5070f
SergioTavares <112196923+SergioTavaresJ@users.noreply.github.com>
2022-10-18 21:32:37 -0300

    Add files via upload

--- Aula 10/Atividade/Aula 10 Q07.py
+++ Aula 10/Atividade/Aula 10 Q07.py
@@ -13,3 +13,11 @@ for n in range(10):
 
 print(v1)
 print(v2)
+
+# Criar a lógica para comparar se um número 
+# é igual e está na mesma posição
+
+for i in range(len(v1)):
+    if v1[i] ==v2[i]:
+        print(f"O valor {v1[i]} e {v2[i]} são iguais!")
+        print(f"Estão os dois na mesma posição {i}")
